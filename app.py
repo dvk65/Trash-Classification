e3ac06e69f7e9d32e4f873dbf3f432dce6dbf834
dipti <kulkarnidipti39@gmail.com>
2025-11-07 17:15:46 -0500

    requirements update

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import tensorflow as tf
-# from PIL import Image
-# import numpy as np
+from PIL import Image
+import numpy as np
 import os
 
 st.write("# Trash Classification App")
@@ -9,10 +9,6 @@ st.write("# Trash Classification App")
 st.write("Current working directory:", os.getcwd())
 st.write("Files here:", os.listdir())
 
-# pillow
-# numpy
-# keras
-# huggingface-hub
 @st.cache_resource
 def load_model():
     return tf.keras.models.load_model("trashclassify.keras")
@@ -25,30 +21,30 @@ camera_photo = st.camera_input("Take a photo")
 # --- File uploader ---
 uploaded_photo = st.file_uploader("Or upload an image", type=["jpg", "jpeg", "png"])
 
-# # --- Use whichever input is available ---
-# if camera_photo is not None:
-#     uploaded_file = camera_photo
-# elif uploaded_photo is not None:
-#     uploaded_file = uploaded_photo
-# else:
-#     uploaded_file = None
+# --- Use whichever input is available ---
+if camera_photo is not None:
+    uploaded_file = camera_photo
+elif uploaded_photo is not None:
+    uploaded_file = uploaded_photo
+else:
+    uploaded_file = None
 
-# # --- Display image if available ---
-# if uploaded_file:
-#     image = Image.open(uploaded_file).convert("RGB")
-#     st.image(image, caption="Your Image", use_column_width=True)
+# --- Display image if available ---
+if uploaded_file:
+    image = Image.open(uploaded_file).convert("RGB")
+    st.image(image, caption="Your Image", use_column_width=True)
 
-# if uploaded_file is not None:
-#     image = Image.open(uploaded_file).convert("RGB")
-#     st.image(image, caption="Uploaded Image", use_column_width=True)
+if uploaded_file is not None:
+    image = Image.open(uploaded_file).convert("RGB")
+    st.image(image, caption="Uploaded Image", use_column_width=True)
 
-#     # Preprocess
-#     img = image.resize((224, 224))  # adjust to your model's input size
-#     img_array = np.array(img) / 255.0
-#     img_array = np.expand_dims(img_array, axis=0)
+    # Preprocess
+    img = image.resize((224, 224))  # adjust to your model's input size
+    img_array = np.array(img) / 255.0
+    img_array = np.expand_dims(img_array, axis=0)
 
-#     # Prediction
-#     prediction = model.predict(img_array)
-#     label = "Recycle" if prediction[0][0] > 0.5 else "General Trash"  # adjust based on your model’s output shape
+    # Prediction
+    prediction = model.predict(img_array)
+    label = "Recycle" if prediction[0][0] > 0.5 else "General Trash"  # adjust based on your model’s output shape
 
-#     st.markdown(f"### 🏷 Prediction: **{label}**")
+    st.markdown(f"### 🏷 Prediction: **{label}**")
